c76275e63abd632dbce90f46d4eb38b469b0b759
jiaoqiqi <1176932165@qq.com>
2016-05-16 19:32:45 +0800

    seven commit

---
+++ src/fibonacci_series.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function fibonacci_series(n) {
+
+    var fibonacciNumers = [];
+
+    fibonacciNumers[0] = 0;
+    fibonacciNumers[i] = 0;
+
+    if (n === 0) {
+        return fibonacciNumers[0];
+    }
+
+    for (var i = 2; ; i++) {
+        if (i > n) {
+            return fibonacciNumers;
+        }
+
+        fibonacciNumers[i] = fibonacciNumers[i - 1] + fibonacciNumers[i - 2];
+    }
+
+}
+
+module.exports = fibonacci_series;
